2ebe83b48632223f64dea6ed2cce5fa29c9e8616
supersem <xsupersemx@gmail.com>
2024-03-31 21:32:09 +0300

    add models, factories, seeds

---
+++ app/Models/Project.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['image_path', 'name', 'description', 'status', 'due_date', 'created_by', 'updated_by'];
+
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
