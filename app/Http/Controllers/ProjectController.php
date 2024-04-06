9a438e06f5185b537d0c90a4caeb72f5a9833a6b
supersem <xsupersemx@gmail.com>
2024-04-07 00:10:04 +0300

    add filtering

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,9 +16,17 @@ class ProjectController extends Controller
     {
         $query = Project::query();
 
+        if (request("name")) {
+            $query->where("name", "like", "%" . request("name") . "%");
+        }
+        if (request("status")) {
+            $query->where("status", request("status"));
+        }
+
         $projects = $query->paginate(10)->onEachSide(1);
         return inertia("Project/Index",[
             "projects" => ProjectResource::collection($projects),
+            'queryParams' => request()->query() ?: null,
         ]);
     }
 
